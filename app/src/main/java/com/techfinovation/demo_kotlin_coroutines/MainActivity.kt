9c95ab1034e59c43c5b6a894ebcb19e2feecb953
ujjval shrimali <ujjvalshri0510@gmail.com>
2022-12-28 23:27:06 +0900

    Initial commit : First Coroutine concept
    Initial commit : First Coroutine concept

---
+++ app/src/main/java/com/techfinovation/demo_kotlin_coroutines/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.techfinovation.demo_kotlin_coroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity";
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // this coroutine will live as long as our application does.
+        GlobalScope.launch {
+            // Suspend thread : it will delay the current coroutine to 3sec
+            // Sleep thread : it will delay or suspend all the coroutine inside the thread.
+            delay(3000L)
+            // this will run different thread.
+            Log.d(TAG, "Coroutines : hello from thread :${Thread.currentThread().name} ");
+        }
+        // this will run on main thread
+        Log.d(TAG, "Hello from main thread :${Thread.currentThread().name} ");
+    }
+}
