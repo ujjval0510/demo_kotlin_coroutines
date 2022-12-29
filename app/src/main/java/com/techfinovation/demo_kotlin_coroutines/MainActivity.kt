043d6395f159a9ee44cda29f72a042df47bc14ba
ujjval shrimali <ujjvalshri0510@gmail.com>
2022-12-29 23:54:42 +0900

    Dispatcher.Main or custom thread in coroutines
    Dispatcher.Main or custom thread in coroutines

--- app/src/main/java/com/techfinovation/demo_kotlin_coroutines/MainActivity.kt
+++ app/src/main/java/com/techfinovation/demo_kotlin_coroutines/MainActivity.kt
@@ -3,6 +3,7 @@ package com.techfinovation.demo_kotlin_coroutines
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -10,22 +11,34 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity";
-    // NOTE : If main thread finish his work that means all other thread and coroutines will be cancelled.
+
+    /**  NOTE : If main thread finish his work that means all other thread and coroutines will be cancelled. **/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // this coroutine will live as long as our application does.
-        GlobalScope.launch {
-            // Suspend thread : it will delay the current coroutine to 3sec
-            // Sleep thread : it will delay or suspend all the coroutine inside the thread.
+        /** this coroutine will live as long as our application does.
+        Dispatchers.Main : Main thread
+        Dispatchers.IO : Data operation such as network call, writing to databases, read/ write files etc.
+        Dispatchers.Unconfined means not confined with any thread.
+        newSingleThreadContext("MyThread) : custom thread **/
+
+        GlobalScope.launch(Dispatchers.Default) {
+
             delay(3000L)
-            // this will run different thread.
+            /** this will run different thread.**/
             Log.d(TAG, "Coroutines : hello from thread :${Thread.currentThread().name} ");
         }
-        // this will run on main thread
+
+        /** this will run on main thread **/
         Log.d(TAG, "Hello from main thread :${Thread.currentThread().name} ");
+
+    }
+
+    suspend fun doNetworkCall(): String {
+        delay(3000L)
+        return "Network call function with 3sec delay ."
     }
 
 }
