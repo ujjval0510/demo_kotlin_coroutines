f34a2931ba1a54904302af0273d0875fa54f67b3
ujjval shrimali <ujjvalshri0510@gmail.com>
2022-12-31 15:30:57 +0900

    explanation about async and await function
    explanation about async and await function

--- app/src/main/java/com/techfinovation/demo_kotlin_coroutines/MainActivity.kt
+++ app/src/main/java/com/techfinovation/demo_kotlin_coroutines/MainActivity.kt
@@ -3,29 +3,99 @@ package com.techfinovation.demo_kotlin_coroutines
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity";
-    // NOTE : If main thread finish his work that means all other thread and coroutines will be cancelled.
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // this coroutine will live as long as our application does.
-        GlobalScope.launch {
-            // Suspend thread : it will delay the current coroutine to 3sec
-            // Sleep thread : it will delay or suspend all the coroutine inside the thread.
-            delay(3000L)
-            // this will run different thread.
-            Log.d(TAG, "Coroutines : hello from thread :${Thread.currentThread().name} ");
-        }
-        // this will run on main thread
-        Log.d(TAG, "Hello from main thread :${Thread.currentThread().name} ");
+        /**the below code will execute after 6 sec when both network
+         *  call complete there process
+         *  but that's the problem, How to resolve that ? **/
+        funNetworkCall()
+
+        /**We will create two different coroutines inside the global scope
+         * then use join method for both jobs then it will take only 3 sec for execute both process
+         * ===============
+         * But this is not good practice for that what should we do ? **/
+        funNetworkDifferentCoroutine()
+
+        /** we can use async method to do that **/
+
+        funAsync()
     }
 
+
+    /** Always use async if we want to use coroutines that return some kind of results **/
+    private fun funAsync() {
+        GlobalScope.launch(Dispatchers.IO) {
+            /**This measureTimeMillis will measure the exact time taken by the coroutine **/
+            val time = measureTimeMillis {
+
+                var answer1 = async { networkCall1() }
+                var answer2 = async { networkCall2() }
+
+                Log.d(TAG, "networkCall1 answer 1 :${answer1.await()} ")
+                Log.d(TAG, "networkCall2 answer 2 :${answer2.await()} ")
+            }
+
+            /**this will also take only 3sec for execute both methods**/
+            Log.d(TAG, "Request time :$time ms ")
+        }
+    }
+
+    private fun funNetworkDifferentCoroutine() {
+        GlobalScope.launch(Dispatchers.IO) {
+            /**This measureTimeMillis will measure the exact time taken by the coroutine **/
+            val time = measureTimeMillis {
+
+                var answer1: String? = null
+                var job1 = launch { answer1 = networkCall1() }
+
+                var answer2: String? = null
+                var job2 = launch { answer2 = networkCall2() }
+
+                job1.join()
+                job2.join()
+
+                Log.d(TAG, "networkCall1 answer 1 :${answer1} ")
+
+                Log.d(TAG, "networkCall2 answer 2 :${answer2} ")
+            }
+
+            /**this will take only 3sec for execute both methods**/
+            Log.d(TAG, "Request time :$time ms ")
+        }
+    }
+
+    private fun funNetworkCall() {
+        GlobalScope.launch(Dispatchers.IO) {
+            /**This measureTimeMillis will measure the exact time taken by the coroutine **/
+            val time = measureTimeMillis {
+
+                val answer1 = networkCall1()
+                val answer2 = networkCall2()
+
+                Log.d(TAG, "networkCall1 answer 1 :${answer1} ")
+                Log.d(TAG, "networkCall2 answer 2 :${answer2} ")
+            }
+            /**this will take 6sec for execute both methods**/
+            Log.d(TAG, "Request time :$time ms ")
+        }
+    }
+
+    private suspend fun networkCall1(): String {
+        delay(3000L)
+        return "Network call 1"
+    }
+
+    private suspend fun networkCall2(): String {
+        delay(3000L)
+        return "Network call 2"
+    }
 }
