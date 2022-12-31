a420a64820583ef71e40ab61140c5db95b44e5e9
ujjval shrimali <ujjvalshri0510@gmail.com>
2022-12-31 16:22:46 +0900

    lifecycleScope will coroutines concept
    lifecycleScope : will destroy all coroutines once’s activity finish
    viewModelScope : will destroy all coroutines once’s model finish (coroutines alive as long as view model alive)

--- app/src/main/java/com/techfinovation/demo_kotlin_coroutines/MainActivity.kt
+++ app/src/main/java/com/techfinovation/demo_kotlin_coroutines/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.techfinovation.demo_kotlin_coroutines
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -12,20 +16,40 @@ class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity";
     // NOTE : If main thread finish his work that means all other thread and coroutines will be cancelled.
 
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // this coroutine will live as long as our application does.
-        GlobalScope.launch {
-            // Suspend thread : it will delay the current coroutine to 3sec
-            // Sleep thread : it will delay or suspend all the coroutine inside the thread.
-            delay(3000L)
-            // this will run different thread.
-            Log.d(TAG, "Coroutines : hello from thread :${Thread.currentThread().name} ");
+        button.setOnClickListener {
+            /** once's the MainActivity is finished and redirect to second activity
+             * but this coroutine with delay 1sec will still running in background even if this activity is destroyed,
+             * and it causes for garbage collector or memory leak in application,
+             * So what should we do ? **/
+            /* GlobalScope.launch {
+                     while (true) {
+                         delay(1000L)
+                         Log.d(TAG, "Still running...")
+                     }
+             }*/
+
+            /** so for that we will use lifecycleScope
+             * that will destroy or stop all coroutines onces activity destroy **/
+            lifecycleScope.launch {
+                while (true) {
+                    delay(1000L)
+                    Log.d(TAG, "Still running...")
+                }
+            }
+
+            GlobalScope.launch {
+                delay(5000L)
+                Intent(this@MainActivity, SecondActivity::class.java).also {
+                    startActivity(it)
+                    finish()
+                }
+            }
         }
-        // this will run on main thread
-        Log.d(TAG, "Hello from main thread :${Thread.currentThread().name} ");
     }
 
 }
---
+++ app/src/main/java/com/techfinovation/demo_kotlin_coroutines/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.techfinovation.demo_kotlin_coroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
