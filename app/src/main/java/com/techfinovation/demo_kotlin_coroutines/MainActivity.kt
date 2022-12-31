0ce23f8c7f4519d16e9dd376c0f6d4e2e4b0e322
ujjval shrimali <ujjvalshri0510@gmail.com>
2022-12-31 12:58:53 +0900

    run blocking concept
    run blocking concept

--- app/src/main/java/com/techfinovation/demo_kotlin_coroutines/MainActivity.kt
+++ app/src/main/java/com/techfinovation/demo_kotlin_coroutines/MainActivity.kt
@@ -6,26 +6,48 @@ import android.util.Log
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
+/** runBlocking **/
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity";
-    // NOTE : If main thread finish his work that means all other thread and coroutines will be cancelled.
+
+    /** NOTE : If main thread finish his work that means all other thread and coroutines will be cancelled. **/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // this coroutine will live as long as our application does.
+        /**
+         * We can not use delay function outside the Global scope / suspend function
+         * that will actually create a new thread not run in main thread
+         * that will also not be blocking our UI.
+         */
+        // delay(3000L) Not use gives compile error
         GlobalScope.launch {
-            // Suspend thread : it will delay the current coroutine to 3sec
-            // Sleep thread : it will delay or suspend all the coroutine inside the thread.
             delay(3000L)
-            // this will run different thread.
-            Log.d(TAG, "Coroutines : hello from thread :${Thread.currentThread().name} ");
+            /** this will run different thread.**/
+            Log.d(TAG, "Coroutines : hello from thread :${Thread.currentThread().name} ")
         }
-        // this will run on main thread
-        Log.d(TAG, "Hello from main thread :${Thread.currentThread().name} ");
+
+        /**
+         * So what we should do if we want to run delay function without using coroutines or
+         * suspend function / Global scope that time you can use runBlocking function.
+         */
+        /**
+         * runBlocking actually block the main thread
+         * it will block our UI updates.
+         * this will use when you don't need coroutines behaviour
+         * but still want to use suspend function in your main thread.
+         * **/
+        runBlocking {
+            delay(3000L)
+        }
+
+        /**  this will run on main thread **/
+        Log.d(TAG, "Hello from main thread :${Thread.currentThread().name} ")
+
     }
 
 }
